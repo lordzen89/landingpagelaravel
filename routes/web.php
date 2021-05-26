d9814458475e22f1cf287e46c7ffc57e83ffa97d
tomas-e <t.edwardsmoraga@uandresbello.edu>
2021-05-25 23:48:47 -0400

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\FileUpload;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/auth/login', [MainController::class, 'login'])->name('auth.login');
+Route::get('/auth/register', [MainController::class, 'register'])->name('auth.register');
+Route::post('/auth/save', [MainController::class, 'save'])->name('auth.save');
+Route::post('/auth/check', [MainController::class, 'check'])->name('auth.check');
+Route::get('/auth/logout', [MainController::class, 'logout'])->name('auth.logout');
+
+Route::get('/upload-file', [FileUpload::class, 'createForm']);
+
+Route::post('/upload-file', [FileUpload::class, 'fileUpload'])->name('fileUpload');
+
+
+Route::group(['middleware'=>['AuthCheck']], function(){
+    Route::get('/admin/dashboard', [MainController::class, 'dashboard']);
+    }
+);
