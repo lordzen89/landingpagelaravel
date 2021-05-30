f8046aef89165158670e6372459160f12eec8fd6
Camiloignaval <camiloignaval@gmail.com>
2021-05-29 21:43:25 -0400

    modelo definitivo, antes de color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Hugo 0.83.1">
-  <title>Ejercicio Landing Page</title>
+  <title>7enScan</title>
 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
@@ -18,8 +18,16 @@
 
   <style>
     .imgLogo {
-      width: 10%
+      width: 50%
     }
+
+    .imgCarta {
+      object-fit: cover;
+    }
+
+    /* .bntLogin {
+      margin-left: 20px;
+    } */
 
     .bd-placeholder-img {
       font-size: 1.125rem;
@@ -29,11 +37,37 @@
       user-select: none;
     }
 
+    @media (min-width: 576px) {
+      .imgLogo {
+        width: 50%
+      }
+    }
+
     @media (min-width: 768px) {
       .bd-placeholder-img-lg {
         font-size: 3.5rem;
+
+      }
+
+      .imgLogo {
+        width: 40%
       }
     }
+
+    @media (min-width: 992px) {
+      .imgLogo {
+        width: 30%
+      }
+    }
+
+    @media (min-width: 1200px) {
+      .imgLogo {
+        width: 20%
+      }
+    }
+
+    @media (min-width: 1400px) {}
+
 
     .container {
       max-width: 960px;
@@ -102,6 +136,10 @@
       flex: 1;
     }
 
+    nav {
+      background-color: #fff;
+    }
+
     @media (min-width: 768px) {
       .flex-md-equal>* {
         flex: 1;
@@ -114,26 +152,32 @@
 <body>
 
   <header class="site-header sticky-top">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="img/Logo_Marca_real.png" alt="" class="imgLogo"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <button type="button" class="btn btn-primary btn-success p-3">Ingresa aqui</button>
-            </li>
+    <nav>
+      <!-- <div class="container-fluid "> -->
+      <div class="row align-items-center">
+        <div class="col">
+          <div class="d-flex align-content-center my-auto">
 
-          </ul>
+            <img src="/images/Logo_Marca_real.png" alt="" class="d-inline-block align-text-top imgLogo align-self-center">
+
+
+          </div>
+        </div>
+        <div class="col">
+          <div class="d-flex">
+            <button type="button" class="btn btn-primary btn-success p-1 p-sm-2 p-md-3 m-2 btnLogin ms-auto"><strong>Ingresa aqui</strong></button>
+          </div>
         </div>
       </div>
+
+
+      <!-- </div> -->
     </nav>
   </header>
 
   <main>
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <!-- carrousel -->
+    <div id="carouselExampleDark" class="carousel carousel-dark slide d-none d-md-block" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -141,19 +185,19 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="10000">
-          <img src="img/logoarre.png" class="d-block w-50  mx-auto" alt="...">
+          <img src="/images/logoarre.png" class="d-block w-50  mx-auto" alt="...">
           <div class="carousel-caption d-none d-md-block">
           </div>
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-          <img src="img/Digitalización.png" class="d-block w-50  mx-auto" alt="...">
+          <img src="/images/Digitalización.png" class="d-block w-50  mx-auto" alt="...">
           <div class="carousel-caption d-none d-md-block">
             <h5 class="text-white">A SOLO UN CLICK!</h5>
             <p class="text-white">Asi de facil sera acceder a tus documentos</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="img/Seguridad.png" class="d-block w-50  mx-auto" alt="...">
+          <img src="/images/Seguridad.png" class="d-block w-50  mx-auto" alt="...">
           <div class="carousel-caption d-none d-md-block">
             <h5 class="text-white">CON LA MAXIMA SEGURIDAD!</h5>
             <p class="text-white">Ofrecemos los mas altos estandares de seguridad para tus documentos</p>
@@ -170,11 +214,12 @@
       </button>
     </div>
 
-    <div class="container my-4">
+    <!-- cards info -->
+    <!-- <div class="container my-4">
       <div class="row d-flex ">
         <div class="col-6 d-flex justify-content-center">
           <div class="card" style="width: 18rem;">
-            <img src="img/imagen1.jpg" class="card-img-top img-fluid" alt="...">
+            <img src="/images/imagen1.jpg" class="card-img-top img-fluid" alt="...">
             <div class="card-body">
               <p class="card-text">
               <h4>Puedes almacenar tus contratos</h4>
@@ -195,19 +240,19 @@
               <p>Tus documentos quedarán protegidos para que nadie más los vea.</p>
               </p>
             </div>
-            <img src="img/imagen2.jpg" class="card-img-top img-fluid" alt="...">
+            <img src="/images/imagen2.jpg" class="card-img-top img-fluid" alt="...">
           </div>
         </div>
       </div>
 
-    </div>
-    <!-- <div class="container">
+    </div> -->
+    <div class="container my-4">
       <div class="row">
-        <div class="col-6">
+        <div class="col-12 col-sm-6">
           <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="img/imagen1.jpg" alt="..." class="img-fluid">
+              <div class="col-md-4 d-flex">
+                <img src="/images/imagen1.jpg" alt="..." class="img-fluid imgCarta">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
@@ -219,11 +264,11 @@
             </div>
           </div>
         </div>
-        <div class="col-6">
+        <div class="col-12 col-sm-6">
           <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
               <div class="col-md-4 d-flex">
-                <img src="img/imagen2.jpg" alt="..." class="img-fluid">
+                <img src="/images/imagen2.jpg" alt="..." class="img-fluid imgCarta">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
@@ -236,7 +281,56 @@
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
+
+    <!-- SCRUM TEAM -->
+    <div class="container">
+      <h1 class="display-4 text-center mt-5">SCRUM TEAM</h1>
+      <div class="row mb-5">
+        <div class="card-group">
+          <div class="card">
+            <img src="/images/camilo.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Scrum Master</h5>
+              <p class="card-text">Camilo Valenzuela</p>
+
+            </div>
+          </div>
+          <div class="card">
+            <img src="/images/pablo.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Product Owner</h5>
+              <p class="card-text">Pablo Arce</p>
+
+            </div>
+          </div>
+          <div class="card">
+            <img src="/images/mariangel.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Desarrollo</h5>
+              <p class="card-text">Mariangel Mujica</p>
+
+            </div>
+          </div>
+          <div class="card">
+            <img src="/images/julio.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Desarrollo</h5>
+              <p class="card-text">Julio Vasquez</p>
+
+            </div>
+          </div>
+          <div class="card">
+            <img src="/images/tomas.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Desarrollo</h5>
+              <p class="card-text">Tomas Edwards</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- Footer -->
     <footer class="bg-dark text-center text-white">
